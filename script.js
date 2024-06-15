824737ca40897f65bccd5c65fe4f11f39055830e
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2024-06-15 12:06:14 -0400

    changed the form name to an id instead of class. For the script.js file I added 2 new functions. One which will fetch the movie api along with the entered user input for a movie then grabbing the movieId. The second function fetches the same api, but using the movie ID in order to grab all the necessary information, and iterate then render the movies to the DOM.

--- script.js
+++ script.js
@@ -4,24 +4,66 @@ const movieFormInput = document.querySelector("#movie-search")
 const renderedMoviesHtml = document.getElementById('rendered-movies-container')
 
 
-movieFormInput.addEventListener("submit", handleUserInput)
+movieFormInput.addEventListener("submit", handleMovieSelection)
 
-function handleUserInput(e){
-    e.preventDefault()
+function handleUserInput(movies){
+
+    let moviesHtml = ``
+
+    for (let movie of movies){
+        // console.log(movie)
+
+        fetch(`http://www.omdbapi.com/?apikey=e099eae9&i=${movie}`)
+            .then(res => res.json())
+            .then(movieDetails => {
+
+                renderedMoviesHtml.innerHTML +=`
+                <div class="rendered-movies-container">
+                    <img src = "${movieDetails.Poster}">
+                    <div class="title-container">
+                        <h2>${movieDetails.Title}</h2>
+                        <p>Star goes here</p>
+                        <p class="movie-rating">${movieDetails.imdbRating}</p>
+                    </div>
+                    <div class = "time-genre-add-container">
+                        <p class ="movie-runtime">${movieDetails.Runtime}</p>
+                        <p class ="movie-genre">${movieDetails.Genre}</p>
+                        <p>Add the Watchlist FONT AWESOME ICON</p>
+                        <p class="watchlist">Watchlist</p>
+                    </div>
+                    <div class ="movie-plot-container">
+                        <p class="movie-plot">${movieDetails.Plot}</p>
+                    </div>
+                </div
+                `
+            
+            })
+    }
+
+}
+
+
+function handleMovieSelection(e){
+    e.preventDefault(e)
     const movieInput = document.getElementById('movieName')
-    let movieHtml = ``
     
     fetch(`http://www.omdbapi.com/?apikey=e099eae9&s=${movieInput.value}`)
         .then(res => res.json())
         .then(data => {
         
-            for (let movies of data.Search ){
-                movieHtml += `
-                
-                    
+            const grabMovieId = data.Search.map((movies)=> {
+                return movies.imdbID
+            })
 
-                `
-            }
+            handleUserInput(grabMovieId)
 
         })
+
 }
+
+// function renderMoviesHtml(movies){
+//     let movieHtml = ``
+//     console.log(movies)
+
+//     movieHtml += 
+// }
