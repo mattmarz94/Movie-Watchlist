88bcf79f45e338534d4bc2d0d5363d8492cee6d6
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2024-06-17 13:34:40 -0400

    cleaned the styling and html, and added an add button to watchlist, and a star for star rating.

--- script.js
+++ script.js
@@ -23,13 +23,13 @@ function handleUserInput(movies){
                     <div class="movie-container">
                         <div class="title-container">
                             <h2>${movieDetails.Title}</h2>
-                            <p>Star goes here</p>
+                            <i class="fa-solid fa-star"></i>
                             <p class="movie-rating">${movieDetails.imdbRating}</p>
                         </div>
                         <div class = "time-genre-add-container">
                             <p class ="movie-runtime">${movieDetails.Runtime}</p>
                             <p class ="movie-genre">${movieDetails.Genre}</p>
-                            <p>Add the Watchlist FONT AWESOME ICON</p>
+                            <i class="fa-solid fa-circle-plus"></i>
                             <p class="watchlist">Watchlist</p>
                         </div>
                         <div class ="movie-plot-container">
