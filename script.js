5c1daf8cbc85a4d4756b378538b4fbf4e84ee3a5
Matt Marzano <122555133+mattmarz94@users.noreply.github.com>
2024-06-19 14:33:30 -0400

    changed the functions created as there were issues attempting to render a new set of movies after the chosen movies at the start.

--- script.js
+++ script.js
@@ -1,72 +1,87 @@
 // API Key e099eae9
 const searchBtn = document.getElementById("search-btn")
 const movieFormInput = document.querySelector("#movie-search")
-const renderedMoviesHtml = document.getElementById('rendered-movies-container')
+const renderedMoviesHtml = document.querySelector('.rendering-movies-container')
 
+let moviesArray = []
 
-movieFormInput.addEventListener("submit", handleMovieSelection)
-
-function handleUserInput(movies){
-
-    let moviesHtml = ``
-
-    for (let movie of movies){
-        // console.log(movie)
-
-        fetch(`http://www.omdbapi.com/?apikey=e099eae9&i=${movie}`)
-            .then(res => res.json())
-            .then(movieDetails => {
-
-                renderedMoviesHtml.innerHTML +=`
-                <div class="rendered-movies-container">
-                    <img src = "${movieDetails.Poster}">
-                    <div class="movie-container">
-                        <div class="title-container">
-                            <h2>${movieDetails.Title}</h2>
-                            <i class="fa-solid fa-star"></i>
-                            <p class="movie-rating">${movieDetails.imdbRating}</p>
-                        </div>
-                        <div class = "time-genre-add-container">
-                            <p class ="movie-runtime">${movieDetails.Runtime}</p>
-                            <p class ="movie-genre">${movieDetails.Genre}</p>
-                            <i class="fa-solid fa-circle-plus"></i>
-                            <p class="watchlist">Watchlist</p>
-                        </div>
-                        <div class ="movie-plot-container">
-                            <p class="movie-plot">${movieDetails.Plot}</p>
-                        </div>
-                    </div>
-                </div>
-                <hr/>
-                `
-            
-            })
+document.addEventListener('click', (e) => {
+    if(e.target.dataset.watchlist){
+        addToWatchlist(e.target.dataset.watchlist)
     }
+})
+
+async function handleUserInput(movies){
+    moviesArray = []
+    for (let movie of movies) {
+        if (!moviesArray.some((m) =>
+            m.Title === movie.Title
+            )) {
+                await fetch(`http://www.omdbapi.com/?apikey=e099eae9&t=${movie.Title}`)
+                    .then(res => res.json())
+                    .then(data => {
+                        moviesArray.push(data)
+                    })
+            }
+    }
+    renderMovies()
+}
+
+function renderMovies(){
+    let moviesHtml = ""
+
+    for (let movie of moviesArray){
+            moviesHtml +=`
+        <div class="rendered-movies-container">
+            <img src = "${movie.Poster}">
+            <div class="movie-container">
+                <div class="title-container">
+                    <h2>${movie.Title}</h2>
+                    <i class="fa-solid fa-star"></i>
+                    <p class="movie-rating">${movie.imdbRating}</p>
+                </div>
+                <div class = "time-genre-add-container">
+                    <p class ="movie-runtime">${movie.Runtime}</p>
+                    <p class ="movie-genre">${movie.Genre}</p>
+                    <i class="fa-solid fa-circle-plus" data-watchlist="${movie.imdbID}"></i>
+                    <p class="watchlist">Watchlist</p>
+                </div>
+                <div class ="movie-plot-container">
+                    <p class="movie-plot">${movie.Plot}</p>
+                </div>
+            </div>
+        </div>
+        <hr/>
+        `
+    }
+    renderedMoviesHtml.innerHTML = moviesHtml
 
 }
 
+// function addToWatchlist(myWatchlist){
 
-function handleMovieSelection(e){
-    e.preventDefault(e)
-    const movieInput = document.getElementById('movieName')
-    
-    fetch(`http://www.omdbapi.com/?apikey=e099eae9&s=${movieInput.value}`)
-        .then(res => res.json())
-        .then(data => {
+//     let stringWatchlist = JSON.stringify(myWatchlist)
+//     console.log(stringWatchlist)
+
+
+//     // localStorage.setItem("My Watchlist", JSON.stringify(myWatchlist))
+// }
+
+
+if (movieFormInput){
+    movieFormInput.addEventListener('submit', async (e) => {
+        e.preventDefault(e)
+        const movieInput = document.getElementById('movieName')
         
-            const grabMovieId = data.Search.map((movies)=> {
-                return movies.imdbID
+        await fetch(`http://www.omdbapi.com/?apikey=e099eae9&s=${movieInput.value}`)
+            .then(res => res.json())
+            .then(data => {
+                handleUserInput(data.Search)
             })
-
-            handleUserInput(grabMovieId)
-
-        })
-
+    })
 }
 
-// function renderMoviesHtml(movies){
+// function renderMoviesHtml(){
 //     let movieHtml = ``
-//     console.log(movies)
-
-//     movieHtml += 
+    
 // }
